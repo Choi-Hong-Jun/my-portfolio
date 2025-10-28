5053dcf2387b0f32c66c1325ab22e367218345e9
Harry <165064577+Choi-Hong-Jun@users.noreply.github.com>
2025-10-28 14:22:59 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,8 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gray-900 text-white">
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gray-900 text-white">
       <h1 className="text-4xl font-bold mb-4">Hello, Hongjun!</h1>
       <p className="text-lg">My Portfolio is under construction 🚧</p>
     </main>
-    </div>
   );
 }
